38acda36b0efaaaa0d2911e0de691a35aa1d5dd0
Dokky <107966608+dokky41@users.noreply.github.com>
2023-10-11 21:44:13 +0900

    (2023.10.11) 캐릭터 이동
    enum을 사용한 캐릭터 이동,
    장애물과 캐릭터 콜라이더 장착

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,87 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public enum RoadLine
+{
+    LEFT = -1,
+    MIDDLE = 0,
+    RIGHT = 1
+}
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] float positionX = 3.5f;
+    [SerializeField] RoadLine roadLine;
+
+    void Start()
+    {
+        roadLine = RoadLine.MIDDLE;
+
+    }
+
+    
+    void Update()
+    {
+        // 캐릭터 이동 함수
+        Move();
+
+        // 캐릭터 이동 상태
+        Status();
+
+    }
+
+    public void Move()
+    {
+        
+        // 왼쪽 방향키를 눌렀을 때
+        if(Input.GetKeyDown (KeyCode.LeftArrow))
+        {
+            if(roadLine == RoadLine.LEFT)
+            {
+                roadLine = RoadLine.LEFT;
+            }
+            else
+            {
+                roadLine--;
+            }
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow)) // 오른쪽
+        {
+
+            if (roadLine == RoadLine.RIGHT)
+            {
+                roadLine = RoadLine.RIGHT;
+            }
+            else
+            {
+                roadLine++;
+            }
+        }
+
+
+        
+
+
+    }
+
+    public void Status()
+    {
+        switch(roadLine)
+        {
+            case RoadLine.LEFT:
+                transform.position = new Vector3(-positionX, 0, 0);
+                break;
+
+            case RoadLine.MIDDLE:
+                transform.position = Vector3.zero;
+                break;
+
+            case RoadLine.RIGHT:
+                transform.position = new Vector3(positionX, 0, 0);
+                break;
+        }
+    }
+
+}
